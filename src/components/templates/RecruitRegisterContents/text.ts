b0fa3550eb32d913e9c92499e85ce2a91688376d
h-yes-oo <hyesoo5115@naver.com>
2020-09-21 07:42:02 +0900

    RecruitRegisterPage basic

---
+++ src/components/templates/RecruitRegisterContents/text.ts
@@ -0,0 +1,19 @@
+export default {
+    clubName: {
+        title: '동아리 이름',
+        description: '내가 담당자로 등록된 동아리만 선택이 가능합니다',
+    },
+    recruitTitle: {
+        title: '공고 제목',
+        description: `공고 제목을 입력하세요`
+    },        
+    recruitPeriod: {
+        title: '모집 기간',
+        description1: '모집 시작 날짜를 선택해주세요',
+        description2: '모집 마감 날짜를 선택해주세요',
+    },
+    recruitDetails: {
+        title: '상세 내용',
+        description: `\n모집 공고를 최대한 상세히 올려주세요\n\n예> 모집 인원, 모집 대상, 지원 방법`
+    }
+}
