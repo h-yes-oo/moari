b33998cc63bdd9ea3746261fde7de32d9f479a7b
h-yes-oo <hyesoo5115@naver.com>
2021-01-29 21:03:50 +0900

    new design for tabs

--- src/constants/palette.ts
+++ src/constants/palette.ts
@@ -4,6 +4,8 @@ const palette = {
     greyText: new Color('#1F2041').alpha(0.75),
     greyNumber: new Color('#1F2041').alpha(0.5),
     greyBorder: new Color('#1F2041').alpha(0.25),
+    dark50: new Color('rgba(31, 32, 65, 0.5)'),
+    dark75: new Color('rgba(31, 32, 65, 0.75)'),
     
     primaryViolet: new Color('#7C71F4'),
     primaryGradient: new Color('#BC9CFF'),
